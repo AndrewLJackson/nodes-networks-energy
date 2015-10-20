4c1427c1e9f7ea68cce0df4a2815f157e791700c
Andrew Jackson <a.jackson@tcd.ie>
2015-10-20 16:16:21 +0100

    minor updates to look at correlation in system dynamics

--- test-networks-functions.r
+++ test-networks-functions.r
@@ -265,6 +265,21 @@ for ( i in 1:n.nodes){
 
 
 
+## put (absolute) correlations on the upper panels,
+## with size proportional to the correlations.
+panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- abs(cor(x, y))
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  txt <- paste0(prefix, txt)
+  if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+  text(0.5, 0.5, txt, cex = cex.cor * r)
+}
+
+pairs(control[,2:(n.nodes+1)], lower.panel = panel.smooth, upper.panel = panel.cor)
+
 
 
 
